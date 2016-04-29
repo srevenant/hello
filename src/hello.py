765b20b41fe80c875bdca4807e83463c35ae94c7
s3tqr2w <brandon.gillespie@kuali.co>
2016-04-29 13:31:03 -0600

    initial commit

---
+++ src/hello.py
@@ -0,0 +1,27 @@
+# vim modeline (put ":set modeline" into your ~/.vimrc)
+# vim:set expandtab ts=4 sw=4 ai ft=python:
+
+import os
+import json
+import sys
+import cherrypy
+
+class Hello(object):
+    @cherrypy.expose
+    def hi(self):
+        return "Hiho" + json.dumps(os.environ, indent=2)
+
+    @cherrypy.expose
+    def bye(self):
+        print("You said bye!")
+        sys.exit(0)
+        
+if __name__ == '__main__':
+    conf = {
+        'global':{
+            'server.socket_port': os.environ.get('PORT') or 8080
+        },
+    }
+
+    cherrypy.quickstart(Hello(), '/', conf)
+
